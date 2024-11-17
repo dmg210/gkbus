cdd54fc9f0f9a70f41cf1718634e87f3a31f16f2
dmg210 <dmg@dg21network.com>
2024-11-17 12:31:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
 	name='gkbus_test',
 	packages=find_packages(),
 	version='0.2.6',
-	description='High-level KWP over K-line/CANbus library',
+	description='High level KWP over K-line and CANbus library',
 	author='Dante383',
 	install_requires=['scapy==2.5.0', 'pyserial==3.5']
 )
